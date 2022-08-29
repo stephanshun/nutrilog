be6bfa1372daaecffd997e5f8d158ded5fc1dd7e
stephanshun <30567591+stephanshun@users.noreply.github.com>
2022-08-30 09:12:01 +1000

    moved rest api + DB from json-server to Axios/MockAPI

--- src/App.js
+++ src/App.js
@@ -3,45 +3,49 @@ import Header from './components/Header'
 import Foods from './components/Foods'
 import AddFood from './components/AddFood'
 import NutritionTotal from './components/NutritionTotal'
+import axios from 'axios'
 
 const App = () => {
   const [showAddFood, setShowAddFood] = useState(false)
   const [foods, setFoods] = useState([])
   var totalProtein = 0
   var totalEnergy = 0
+  const url = 'https://630c789b53a833c5342c229e.mockapi.io/foods'
+
+  // Calculate total kJs and Protein info (constantly)
+  nutritionCalc()
 
   useEffect(() => {
-    const getFoods = async () => {
-      const foodsFromServer = await fetchFoods()
-      setFoods(foodsFromServer)
-    }
-
-    getFoods()
+    axios.get(url)
+    .then(response => {
+        setFoods(response.data)
+    })
   }, [])
 
   // Fetch Tasks
-  const fetchFoods = async () => {
-    const res = await fetch('http://localhost:5000/foods')
-    const data = await res.json()
+  // const fetchFoods = async () => {
+  //   const res = await fetch('http://localhost:5000/foods')
+  //   const data = await res.json()
 
-    return data
-  }
+  //   return data
+  // }
 
-  nutritionCalc()
+
 
   // Add Food
   const addFood = async (food) => {
-    const res = await fetch('http://localhost:5000/foods', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(food),
-    })
+    const response = await axios.post(url, food);
+    // await fetch('http://localhost:5000/foods', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-type': 'application/json'
+    //   },
+    //   body: JSON.stringify(food),
+    // })
 
-    const data = await res.json()
+    // const data = await res.json()
 
-    setFoods([...foods, data])
+    setFoods([...foods, response.data])
 
     // const id = Math.floor(Math.random() * 10000) + 1
     // const newFood = { id, ...food }
@@ -50,9 +54,11 @@ const App = () => {
 
   // Delete Food
   const deleteFood = async (id) => {
-    await fetch(`http://localhost:5000/foods/${id}`, {
-      method: 'DELETE'
-    })
+    var url_id = url + `/${id}`;
+    await axios.delete(url_id);
+    // await fetch(`http://localhost:5000/foods/${id}`, {
+    //   method: 'DELETE'
+    // })
 
     setFoods(foods.filter((food) => food.id !== id))
   }
